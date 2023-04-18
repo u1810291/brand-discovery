da9bd8f6becd380e17b31750d509cd9a0b4dc2a0
Александр Чусовитин <aleksandrcusovitin@MacBook-Pro-Aleksandr.local>
2023-04-19 00:01:51 +0600

    feat: add initial and limit modals

---
+++ src/components/InitialModal/index.ts
@@ -0,0 +1,1 @@
+export { InitialModal } from './InitialModal'
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { InitialModal } from './InitialModal'
+export { LimitModal } from './LimitModal'
 export { TabsPanel } from './TabsPanel'
