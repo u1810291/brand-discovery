b976c9e2380f0e718c726d6299f433b59f0de14b
Александр Чусовитин <aleksandrcusovitin@MacBook-Pro-Aleksandr.local>
2023-05-02 20:15:59 +0600

    feat: add layout info tabs

--- src/pages-components/Home/Home.tsx
+++ src/pages-components/Home/Home.tsx
@@ -1,24 +1,22 @@
 'use client'
 
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import HomeIcon from '@mui/icons-material/Home'
 import InfoIcon from '@mui/icons-material/Info'
 import SettingsIcon from '@mui/icons-material/Settings'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import Button from '@mui/material/Button'
 import { getAuth } from 'firebase/auth'
 import { useRouter } from 'next/router'
-import { useDispatch } from 'src/store'
 import { useEffect, useState } from 'react'
-import { ROUTES } from 'src/constants/routes'
-import { TabsPanel } from 'src/components/TabsPanel'
 import { useSignOut } from 'react-firebase-hooks/auth'
-import { closeModal, openModal } from 'src/store/slices/modal'
-import { SettingsPageContent } from './components/SettingsPageContent'
-import { UserData } from 'src/store/slices/auth/auth.slice'
+import { TabsPanel } from 'src/components/TabsPanel'
+import { ROUTES } from 'src/constants/routes'
 import { MainLayout } from 'src/layouts/MainLayout'
 import firebaseApp from 'src/services/firebase'
-import { HomePageContent, LikedPageContent } from './components'
+import { useDispatch } from 'src/store'
+import { UserData } from 'src/store/slices/auth/auth.slice'
+import { closeModal, openModal } from 'src/store/slices/modal'
+import { HomePageContent, InfoPageContent, LikedPageContent, SettingsPageContent } from './components'
 import { companies } from './mock'
 
 const auth = getAuth(firebaseApp())
@@ -68,14 +66,14 @@ export const Home = () => {
       ),
     },
     { icon: <FavoriteBorderIcon />, content: <LikedPageContent data={companies} /> },
-    { icon: <InfoIcon />, content: <Box>Info</Box> },
+    { icon: <InfoIcon />, content: <InfoPageContent /> },
     {
       icon: <SettingsIcon />,
       content: <SettingsPageContent setSuccess={setSuccess} signOut={signOut} loading={loading} />,
     },
   ]
   return (
-    <MainLayout sx={{ background: '#F8F9FB' }}>
+    <MainLayout sx={{ background: '#F8F9FB' }} padding={0}>
       <TabsPanel tabs={tabs} error={errorMessage?.message} success={success} />
       {/* <LimitModal open={isLimitModalOpen} /> */}
     </MainLayout>
--- src/pages-components/Home/components/index.ts
+++ src/pages-components/Home/components/index.ts
@@ -1,2 +1,4 @@
 export { HomePageContent } from './HomePageContent'
+export { InfoPageContent } from './InfoPageContent'
 export { LikedPageContent } from './LikedPageContent'
+export { SettingsPageContent } from './SettingsPageContent'
