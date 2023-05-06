edb5d59453b45729d099a96a4d52a73c3f9f1987
u1810291 <otabekbutcher@gmail.com>
2023-05-06 14:18:02 +0500

    fix: fixed build chunks

---
+++ src/pages-components/Link/Link.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { getAuth } from 'firebase/auth'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { ROUTES } from 'src/constants/routes'
+import { useVerifyEmail } from 'src/services/useVerifyEmail'
+import firebaseApp from 'src/services/firebase'
+
+const auth = getAuth(firebaseApp())
+
+export const Link = () => {
+  const router = useRouter()
+  const [verifyEmail] = useVerifyEmail(auth)
+  useEffect(() => {
+    if (router.query.mode === 'verifyEmail') {
+      if (router?.query?.oobCode) {
+        verifyEmail(router?.query?.oobCode)
+        router.replace(ROUTES.signIn)
+      }
+    }
+    if (router.query.mode === 'resetPassword') {
+      router.push(`${ROUTES.newPassword}${window.location.search}`)
+    }
+  }, [router.query.mode])
+  return null
+}
