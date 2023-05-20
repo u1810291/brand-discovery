66c76d804b522927b9eff1a042acc2be252b26a2
Александр Чусовитин <aleksandrcusovitin@MacBook-Pro-Aleksandr.local>
2023-05-20 14:48:19 +0600

    fix: autoplay in iOS

--- src/pages-components/WalkThrough/WalkThrough.tsx
+++ src/pages-components/WalkThrough/WalkThrough.tsx
@@ -1,9 +1,10 @@
 import Stack from '@mui/material/Stack'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player/file'
 import { GallerySwiper } from 'src/UI/GallerySwiper'
 import { Indicator } from 'src/components/Indicator'
+import { ROUTES } from 'src/constants/routes'
 import { MainLayout } from 'src/layouts/MainLayout'
 import { Swiper as SwiperCommon } from 'swiper'
 
@@ -24,7 +25,6 @@ export const WalkThrough = () => {
       swiper?.slideNext()
       setActiveIndex((prev) => prev + 1)
     } else {
-      alert('end')
       localStorage.setItem('walkthroughCompleted', true.toString())
       router.push(ROUTES.signIn)
     }
@@ -48,7 +48,16 @@ export const WalkThrough = () => {
         swiperOptions={{ spaceBetween: 0, slidesPerView: 1 }}
         setSwiper={setSwiper}
         renderElement={(item) => (
-          <ReactPlayer width="100%" height="100%" muted playing loop onEnded={getNextSlide} url={item.src} />
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            muted
+            playsinline
+            playing
+            loop
+            onEnded={getNextSlide}
+            url={item.src}
+          />
         )}
       />
     </MainLayout>
